b1fb07ace445c89b57c68e25a13aab7d3adac62e
aoife kavanagh <nyhankaa@tcd.ie>
2019-05-31 12:42:56 +0000

    added multiplication script basic

---
+++ multiply.py
@@ -0,0 +1,10 @@
+# initiate variable n to take input of int
+n = int(input("Pick a number from 1 - 30: "))
+
+# use for loop to iterate up to & including the input value
+for i in range(1, n+1):
+    if n >= 30: 
+        print 'Sorry, integer value too high.'
+        break
+    else:
+	   print(n,'x',i,'=',n*i)
