a062c6deb57697d09079c46e6b50cdb2d6c1efa9
aoife kavanagh <nyhankaa@tcd.ie>
2019-05-31 12:50:14 +0000

    completed function

--- multiply.py
+++ multiply.py
@@ -1,10 +1,14 @@
-# initiate variable n to take input of int
-n = int(input("Pick a number from 1 - 30: "))
+def multiply():
 
-# use for loop to iterate up to & including the input value
-for i in range(1, n+1):
-    if n >= 30: 
-        print 'Sorry, integer value too high.'
-        break
-    else:
-	   print(n,'x',i,'=',n*i)
+    # initiate variable n to take input of int
+    n = int(input("Pick a number from 1 - 30: "))
+    
+    # use for loop to iterate up to & including the input value
+    for i in range(1, n+1):
+        if n >= 30: 
+            print 'Sorry, integer value too high.'
+            break
+        else:
+    	   print(n,'x',i,'=',n*i)
+    	
+print multiply()
